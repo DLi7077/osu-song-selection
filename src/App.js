9174504156500ff9b14f5e3a41a5ce3240bb3dbb
butterriolu <derpvin12@gmail.com>
2023-01-13 17:01:10 -0500

    add music audio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Background from "./Components/Background";
 import { SONGS } from "./Constants/songs";
 import SongMenu from "./Components/SongMenu";
+import sleep from "./Utils/sleep";
 
 function App() {
   const [songIndex, setSongIndex] = useState(0);
+  const [audio] = useState(new Audio(SONGS[songIndex].audio));
+
+  async function raiseVolume(audio) {
+    const delay = 400;
+    audio.volume = 0.1;
+    await sleep(delay);
+    audio.volume += 0.1;
+    await sleep(delay);
+    audio.volume += 0.1;
+    await sleep(delay);
+    audio.volume += 0.1;
+  }
+
+
+  useEffect(() => {
+    audio.loop = true;
+
+    audio.setAttribute("src", SONGS[songIndex].audio); //change the source
+    audio.load();
+    audio.currentTime = 48;
+    audio.play();
+    raiseVolume(audio);
+  }, [songIndex]);
 
   return (
     <div>
-      <Background
-        src={SONGS[songIndex].background}
-      />
+      <Background src={SONGS[songIndex].background} />
       <SongMenu
         songs={SONGS}
         updateSelectedIndex={(index) => {
+          if (index === songIndex) return;
           setSongIndex(index);
         }}
       />
--- src/Constants/songs.js
+++ src/Constants/songs.js
@@ -5,6 +5,7 @@ export const SONGS = [
     difficulty: "Mayu's Expert",
     star_rating: 5.42,
     background: require("../Assets/Backgrounds/koko-de-ikiteiru.jpg"),
+    audio: require("../Assets/Audio/koko-de-ikiteiru.mp3"),
   },
   {
     title: "Tada Koe Hitotsu",
@@ -12,6 +13,7 @@ export const SONGS = [
     difficulty: "Expert",
     star_rating: 5.59,
     background: require("../Assets/Backgrounds/tada-koe-hitotsu.png"),
+    audio: require("../Assets/Audio/tada-koe-hitotsu.mp3"),
   },
   {
     title: "Kimi no Shiranai Monogatari",
@@ -19,6 +21,7 @@ export const SONGS = [
     difficulty: "Celestial",
     star_rating: 5.14,
     background: require("../Assets/Backgrounds/kimi-no-shiranai-monogatari.jpg"),
+    audio: require("../Assets/Audio/kimi-no-shiranai-monogatari.mp3"),
   },
   {
     title: "GET BACK",
@@ -26,6 +29,7 @@ export const SONGS = [
     difficulty: "Rolniczy's Insane",
     star_rating: 5.2,
     background: require("../Assets/Backgrounds/get-back.jpg"),
+    audio: require("../Assets/Audio/get-back.mp3"),
   },
   {
     title: "Asu no Yozora Shoukaihan",
@@ -33,6 +37,7 @@ export const SONGS = [
     difficulty: "Linada's Insane",
     star_rating: 5.2,
     background: require("../Assets/Backgrounds/asu-no-yozora-shokaihan.png"),
+    audio: require("../Assets/Audio/asu-no-yozora-shokaihan.mp3"),
   },
   {
     title: "Endless Tears",
@@ -40,6 +45,7 @@ export const SONGS = [
     difficulty: "AlRakuen's Extra",
     star_rating: 5.21,
     background: require("../Assets/Backgrounds/endless-tears.jpg"),
+    audio: require("../Assets/Audio/endless-tears.mp3"),
   },
   {
     title: "Flower Dance",
@@ -47,6 +53,7 @@ export const SONGS = [
     difficulty: "Camellia",
     star_rating: 5.22,
     background: require("../Assets/Backgrounds/flower-dance.jpg"),
+    audio: require("../Assets/Audio/flower-dance.mp3"),
   },
   {
     title: "Blue Zenith",
@@ -54,13 +61,15 @@ export const SONGS = [
     difficulty: "RLC's Insane",
     star_rating: 5.22,
     background: require("../Assets/Backgrounds/blue-zenith.jpg"),
+    audio: require("../Assets/Audio/blue-zenith.mp3"),
   },
   {
     title: "Zankyo Sanka (TV Size)",
     artist: "Aimer // Sotarks",
     difficulty: "Expert",
     star_rating: 5.27,
-    background: require("../Assets/Backgrounds/asu-no-yozora-shokaihan.png"),
+    background: require("../Assets/Backgrounds/zankyo-sanka.jpg"),
+    audio: require("../Assets/Audio/zankyo-sanka.mp3"),
   },
   {
     title: "Utsuroi (Short Ver.)",
@@ -68,6 +77,7 @@ export const SONGS = [
     difficulty: "mnyui's Extra",
     star_rating: 5.39,
     background: require("../Assets/Backgrounds/utsuroi-short.jpg"),
+    audio: require("../Assets/Audio/utsuroi-short.mp3"),
   },
   {
     title: "Ano Yume o Nazotte",
@@ -75,6 +85,7 @@ export const SONGS = [
     difficulty: "Daisuki",
     star_rating: 5.53,
     background: require("../Assets/Backgrounds/ano-yume-o-nazotte.jpg"),
+    audio: require("../Assets/Audio/ano-yume-o-nazotte.mp3"),
   },
   {
     title: "Kaikai Kitan",
@@ -82,6 +93,7 @@ export const SONGS = [
     difficulty: "FuJu's Expert",
     star_rating: 5.55,
     background: require("../Assets/Backgrounds/kaikai-kitan.png"),
+    audio: require("../Assets/Audio/kaikai-kitan.mp3"),
   },
   {
     title: "LEVEL <yanaginagi ver.>",
@@ -89,6 +101,7 @@ export const SONGS = [
     difficulty: "Gaia's Extra",
     star_rating: 5.57,
     background: require("../Assets/Backgrounds/level-yanaginagi-ver.jpg"),
+    audio: require("../Assets/Audio/level-yanaginagi-ver.mp3"),
   },
   {
     title: "Kani*Do-Luck! (TV Size)",
@@ -96,6 +109,7 @@ export const SONGS = [
     difficulty: "Extra!",
     star_rating: 5.66,
     background: require("../Assets/Backgrounds/kani-do-luck.jpg"),
+    audio: require("../Assets/Audio/kani-do-luck.mp3"),
   },
   {
     title: "Monochrome Butterfly",
@@ -103,6 +117,7 @@ export const SONGS = [
     difficulty: "Lami's Extra",
     star_rating: 5.88,
     background: require("../Assets/Backgrounds/monochrome-butterfly.jpg"),
+    audio: require("../Assets/Audio/monochrome-butterfly.mp3"),
   },
   {
     title: "Literature",
@@ -110,5 +125,6 @@ export const SONGS = [
     difficulty: "Promenade",
     star_rating: 5.96,
     background: require("../Assets/Backgrounds/literature.jpg"),
+    audio: require("../Assets/Audio/literature.mp3"),
   },
 ];
