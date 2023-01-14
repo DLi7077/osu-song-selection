de0427120201eea19f5896911f0280fbe1a910c6
butterriolu <derpvin12@gmail.com>
2023-01-14 00:58:52 -0500

    add hover sound

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Background from "./Components/Background";
 import { SONGS } from "./Constants/songs";
 import SongMenu from "./Components/SongMenu";
 import sleep from "./Utils/sleep";
+import hoverSound from "./Assets/Audio/song-hover.wav";
 
 function App() {
   const [songIndex, setSongIndex] = useState(0);
   const [audio, setAudio] = useState(SONGS[songIndex].audio);
+  const hoverAudio = new Audio(hoverSound);
+  hoverAudio.volume = 0.5;
 
   async function raiseVolume(audio) {
     const delay = 100;
@@ -17,6 +20,10 @@ function App() {
         audio.volume += 0.025;
       });
     }
+  }
+
+  function playHoverSound() {
+    hoverAudio.play();
   }
 
   useEffect(() => {
@@ -35,11 +42,11 @@ function App() {
         songIndex={songIndex}
         updateSelectedIndex={(index) => {
           if (index === songIndex) return;
-
           audio.pause();
           setSongIndex(index);
           setAudio(SONGS[index].audio); //change the source
         }}
+        playHoverSound={playHoverSound}
       />
     </div>
   );
