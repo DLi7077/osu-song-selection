19d8cdc2cb69044c0debf555813de21d758ead55
butterriolu <derpvin12@gmail.com>
2023-01-13 23:47:32 -0500

    adjust background movement

--- src/Components/Background/index.js
+++ src/Components/Background/index.js
@@ -16,20 +16,21 @@ export default function Background({ src }) {
   }, [mousePos]);
 
   // max scale 10%, spare 2.5% for vertical and horizontal
-  const MAX_MARGIN_PERCENT = 2.5;
+  const MAX_MARGIN_PERCENT = 1  ;
   const XPosOffset = (xPos) => (xPos * MAX_MARGIN_PERCENT) / 1920;
   const YPosOffset = (yPos) => (yPos * MAX_MARGIN_PERCENT) / 1080;
 
   const classes = {
     background: {
       objectFit: "cover",
-      transform: `scale(1.075) translate(-${XPosOffset(
+      objectPosition: "center center",
+      transform: `scale(1.02) translate(-${XPosOffset(
         mousePos.x
       )}%, -${YPosOffset(mousePos.y)}%)`,
       position: "fixed",
       zIndex: -2,
       width: "100%",
-      filter: 'brightness(70%)'
+      filter: "brightness(70%)",
     },
   };
 
