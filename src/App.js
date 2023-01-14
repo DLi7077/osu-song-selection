6707718f4f313c05e635ffc8d4e6c968eaaed62b
butterriolu <derpvin12@gmail.com>
2023-01-14 09:57:35 -0500

    move audio logic

--- src/App.js
+++ src/App.js
@@ -2,29 +2,11 @@ import { useState, useEffect } from "react";
 import Background from "./Components/Background";
 import { SONGS } from "./Constants/songs";
 import SongMenu from "./Components/SongMenu";
-import sleep from "./Utils/sleep";
-import hoverSound from "./Assets/Audio/song-hover.wav";
+import useAudio from "./Hooks/useAudio";
 
 function App() {
   const [songIndex, setSongIndex] = useState(0);
-  const [audio, setAudio] = useState(SONGS[songIndex].audio);
-  const hoverAudio = new Audio(hoverSound);
-  hoverAudio.volume = 0.5;
-
-  async function raiseVolume(audio) {
-    const delay = 100;
-    audio.volume = 0.01;
-
-    for (let i = 0; i < 10; i++) {
-      await sleep(delay).then(() => {
-        audio.volume += 0.025;
-      });
-    }
-  }
-
-  function playHoverSound() {
-    hoverAudio.play();
-  }
+  const { audio, setAudio, raiseVolume, playHoverSound } = useAudio();
 
   useEffect(() => {
     audio.loop = true;
