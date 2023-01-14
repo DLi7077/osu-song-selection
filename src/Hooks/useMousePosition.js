179fcbd76ac14f632df4166bb33a2bdcc2d34dc3
butterriolu <derpvin12@gmail.com>
2023-01-14 10:22:36 -0500

    add mouse position hook

---
+++ src/Hooks/useMousePosition.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export default function useMousePosition() {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [mousePos]);
+
+  return mousePos;
+}
